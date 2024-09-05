f3b8e6af8b34f8a962d6531429d22c23fb8352a6
Legendfox <rocketheadvids@gmail.com>
2024-09-05 15:08:51 -0500

    revamping dropdown, added basic javascript

---
+++ main/js/home-script.js
@@ -0,0 +1,9 @@
+var redirectTest = document.getElementById("redirect-test").textContent
+
+function redirectToPage() {
+ window.location.href = redirectTest;
+ console.log("redirected")
+}
+
+
+console.log(redirectTest + " <<- this is the redirectTest value");
