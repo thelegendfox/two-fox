1faff24f214a98b34210bcc108af89a16ef50d95
Legendfox <rocketheadvids@gmail.com>
2024-09-06 13:16:16 -0500

    preparation for complete revamp of main page

--- main/js/home-script.js
+++ main/js/home-script.js
@@ -1,9 +1,19 @@
-var redirectTest = document.getElementById("redirect-test").textContent
-
-function redirectToPage() {
- window.location.href = redirectTest;
- console.log("redirected")
-}
 
 
-console.log(redirectTest + " <<- this is the redirectTest value");
+
+
+
+
+
+
+
+
+// var redirectTest = document.getElementById("redirect-test").textContent
+
+// function redirectToPage() {
+//  window.location.href = redirectTest;
+//  console.log("redirected")
+// }
+
+
+// console.log(redirectTest + " <<- this is the redirectTest value");
