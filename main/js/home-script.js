41cd4cc38a1d370a30952afdcfed37bb1fbc1d2f
Legendfox <rocketheadvids@gmail.com>
2024-09-10 12:17:02 -0500

    debug stuff

--- main/js/home-script.js
+++ main/js/home-script.js
@@ -1,19 +1,12 @@
+/*this is really ugly but i dont know another way*/
+/*sorry*/
+
+let page1Button = document.getElementById("page1").value;
+
+function redirectToPage() {
+ window.location.href = page1Button;
+ console.log("redirected")
+}
 
 
-
-
-
-
-
-
-
-
-// var redirectTest = document.getElementById("redirect-test").textContent
-
-// function redirectToPage() {
-//  window.location.href = redirectTest;
-//  console.log("redirected")
-// }
-
-
-// console.log(redirectTest + " <<- this is the redirectTest value");
+console.log(page1Button + " <<- this is the page1Button value"); /*remove before release*/
