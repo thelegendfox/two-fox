a06f88f2472ef6624f3ecbc600a8c040d3e642c0
Legendfox <rocketheadvids@gmail.com>
2024-10-23 11:53:57 -0500

    added all features to rps proj, prep dev > main

--- main/pages/rock-paper-scissors/style.js
+++ main/pages/rock-paper-scissors/style.js
@@ -5,10 +5,10 @@ let computerScore = 0;
 let contextResult = "";
 let contextComputerChoice = "";
 let contextHumanChoice = "";
-let gameRunning = 5;
+let gameRunning = 0;
 let formLabel = document.getElementById("formLabel");
-let roundsRemainingText = document.getElementById("rounds-remaining-text");
-let roundsRemaining = 5;
+let roundsPlayed = 0;
+
 let htmlText1 = document.getElementById("text1");
 let htmlText2 = document.getElementById("text2");
 let htmlText3 = document.getElementById("text3");
@@ -16,14 +16,19 @@ let htmlText4 = document.getElementById("text4");
 let htmlText5 = document.getElementById("text5");
 
 function playGameButton() {
-	if (gameRunning > 0) {
+	if (gameRunning == 0) {
 		playHtmlGame();
+	} else {
+		return;
 	}
 }
 
 function newRoundButton() {
-	playRound();
-	gameRunning--;
+	if (gameRunning > 0) {
+		playHtmlGame();
+	} else {
+		return;
+	}
 }
 
 function getComputerChoice() {
@@ -31,15 +36,15 @@ function getComputerChoice() {
 	if (randomChoice === 1) {
 		console.log("Computer Chooses: Rock! " + randomChoice);
 		contextComputerChoice = "Computer Chooses: Rock!";
-		return "rock";
+		return "Rock";
 	} else if (randomChoice === 2) {
 		console.log("Computer Chooses: Paper! " + randomChoice);
 		contextComputerChoice = "Computer Chooses: Paper!";
-		return "paper";
+		return "Paper";
 	} else if (randomChoice === 3) {
 		console.log("Computer Chooses: Scissors! " + randomChoice);
-		contextComputerChoice = "Computer Chooses: Paper!";
-		return "scissors";
+		contextComputerChoice = "Computer Chooses: Scissors!";
+		return "Scissors";
 	}
 }
 
@@ -58,12 +63,8 @@ function getHumanChoice() {
 		);
 		getHumanChoice();
 	} else {
-		console.log(
-			"Human Chooses: " +
-				humanChoice.toLowerCase().charAt(0).toUpperCase() +
-				humanChoice.slice(1) +
-				"!"
-		);
+		humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+		console.log("Human Chooses: " + humanChoice + "!");
 		return humanChoice;
 	}
 }
@@ -79,42 +80,42 @@ function playRound() {
 	let humanChoice = getHumanChoice();
 	let computerChoice = getComputerChoice();
 
-	if (humanChoice == "rock" && computerChoice == "scissors") {
+	if (humanChoice == "Rock" && computerChoice == "Scissors") {
 		console.log("You win! Rock beats Scissors!");
 		contextResult = "You win! Rock beats Scissors!";
 		humanScore++;
 		result = 1;
-	} else if (humanChoice == "paper" && computerChoice == "rock") {
+	} else if (humanChoice == "Paper" && computerChoice == "Rock") {
 		console.log("You win! Paper beats Rock!");
 		contextResult = "You win! Paper beats Rock!";
 		humanScore++;
 		result = 2;
-	} else if (humanChoice == "scissors" && computerChoice == "paper") {
+	} else if (humanChoice == "Scissors" && computerChoice == "Paper") {
 		console.log("You win! Scissors beats Paper!");
 		contextResult = "You win! Scissors beats Paper!";
 		humanScore++;
 		result = 3;
-	} else if (humanChoice == "rock" && computerChoice == "paper") {
+	} else if (humanChoice == "Rock" && computerChoice == "Paper") {
 		console.log("You lose! Paper beats Rock!");
 		contextResult = "You lose! Paper beats Rock!";
 		computerScore++;
 		result = 4;
-	} else if (humanChoice == "paper" && computerChoice == "scissors") {
+	} else if (humanChoice == "Paper" && computerChoice == "Scissors") {
 		console.log("You lose! Scissors beats Paper!");
 		contextResult = "You lose! Scissors beats Paper!";
 		computerScore++;
 		result = 5;
-	} else if (humanChoice == "scissors" && computerChoice == "rock") {
+	} else if (humanChoice == "Scissors" && computerChoice == "Rock") {
 		console.log("You lose! Rock beats Scissors!");
 		contextResult = "You lose! Rock beats Scissors!";
 		computerScore++;
 		result = 6;
 	} else if (humanChoice == computerChoice) {
 		console.log(
-			"It's a draw! " + humanChoice + "is the same as" + computerChoice + "!"
+			"It's a draw! " + humanChoice + " is the same as " + computerChoice + "!"
 		);
 		contextResult =
-			"It's a draw! " + humanChoice + "is the same as" + computerChoice + "!";
+			"It's a draw! " + humanChoice + " is the same as " + computerChoice + "!";
 		result = 7;
 	}
 }
@@ -157,8 +158,34 @@ function playGame() {
 
 function playHtmlGame() {
 	//text content: The game is affoot, all that stuff above, connect it to PlayRound and the round playing button
-
+	gameRunning++;
 	playRound();
-	roundsRemaining--;
-	roundsRemainingText.textContent = "Rounds remaining: " + roundsRemaining;
+	roundsPlayed++;
+	htmlText1.textContent = contextComputerChoice;
+	htmlText2.textContent = contextResult;
+	htmlText3.textContent = "Your score: " + humanScore;
+	htmlText4.textContent = "Computer score: " + computerScore;
+	htmlText5.textContent = "Rounds played: " + roundsPlayed;
+}
+
+function endGameButton() {
+	if (roundsPlayed > 0) {
+		let winner = "";
+		if (humanScore > computerScore) {
+			winner = "You win!";
+		} else if (computerScore > humanScore) {
+			winner = "The computer wins!";
+		} else {
+			winner = "It's a draw! I don't know if that's unlucky or lucky.";
+		}
+		htmlText1.textContent = "Good game!";
+		htmlText2.textContent = winner;
+		htmlText3.textContent = "Your score: " + humanScore;
+		htmlText4.textContent = "Computer score: " + computerScore;
+		htmlText5.textContent = "Rounds you played: " + roundsPlayed;
+		roundsPlayed = 0;
+		gameRunning = 0;
+	} else {
+		return;
+	}
 }
