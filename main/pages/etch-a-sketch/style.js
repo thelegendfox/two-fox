68c0ac9977b051fda83cbc6d948179540bb12e49
Legendfox <rocketheadvids@gmail.com>
2024-11-01 12:43:22 -0500

    i fucking hate this assignment

--- main/pages/etch-a-sketch/style.js
+++ main/pages/etch-a-sketch/style.js
@@ -5,30 +5,30 @@ const sketchContainer2 = document.querySelector("#container2");
 const sketchContainer3 = document.querySelector("#container3");
 const sketchContainer4 = document.querySelector("#container4");
 function createSketchPage() {
-	for (let i = 0; i < 4; i++) {
+	// for (let i = 0; i < 4; i++) {
+	// 	const element = document.createElement("div");
+	// 	element.classList.add("block-far-left");
+	// 	element.textContent = "TEST";
+	// 	sketchContainer3.appendChild(element);
+	// }
+	for (let i = 0; i < 16; i++) {
 		const element = document.createElement("div");
-		element.classList.add("block");
-		element.textContent = "TEST";
-		sketchContainer1.appendChild(element);
-	}
-	for (let i = 0; i < 4; i++) {
-		const element = document.createElement("div");
-		element.classList.add("block");
-		element.textContent = "TEST";
-		sketchContainer2.appendChild(element);
-	}
-	for (let i = 0; i < 4; i++) {
-		const element = document.createElement("div");
-		element.classList.add("block");
+		element.classList.add("block-left");
 		element.textContent = "TEST";
 		sketchContainer3.appendChild(element);
 	}
-	for (let i = 0; i < 4; i++) {
-		const element = document.createElement("div");
-		element.classList.add("block");
-		element.textContent = "TEST";
-		sketchContainer4.appendChild(element);
-	}
+	// for (let i = 0; i < 8; i++) {
+	// 	const element = document.createElement("div");
+	// 	element.classList.add("block-right");
+	// 	element.textContent = "TEST";
+	// 	sketchContainer3.appendChild(element);
+	// }
+	// for (let i = 0; i < 4; i++) {
+	// 	const element = document.createElement("div");
+	// 	element.classList.add("block-far-right");
+	// 	element.textContent = "TEST";
+	// 	sketchContainer3.appendChild(element);
+	// }
 }
 /**/
 
