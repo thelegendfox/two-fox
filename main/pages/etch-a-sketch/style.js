b3acc080633f06ce63620e3c85aaf2194ada6d02
Legendfox <rocketheadvids@gmail.com>
2024-11-04 13:59:34 -0600

    a

--- main/pages/etch-a-sketch/style.js
+++ main/pages/etch-a-sketch/style.js
@@ -1,35 +1,21 @@
+const sketchContainer = document.querySelector("#container");
+const alphaContainer = document.querySelector("#mainContainer");
+
 /**/
 //Creating grid
-const sketchContainer1 = document.querySelector("#container1");
-const sketchContainer2 = document.querySelector("#container2");
-const sketchContainer3 = document.querySelector("#container3");
-const sketchContainer4 = document.querySelector("#container4");
+
+const totalBlocks = 256; //this must be interactable later
 function createSketchPage() {
-	// for (let i = 0; i < 4; i++) {
-	// 	const element = document.createElement("div");
-	// 	element.classList.add("block-far-left");
-	// 	element.textContent = "TEST";
-	// 	sketchContainer3.appendChild(element);
-	// }
-	for (let i = 0; i < 16; i++) {
+	for (let i = 0; i < totalBlocks; i++) {
 		const element = document.createElement("div");
-		element.classList.add("block-left");
+		element.classList.add("block");
 		element.textContent = "TEST";
-		sketchContainer3.appendChild(element);
+		sketchContainer.appendChild(element);
 	}
-	// for (let i = 0; i < 8; i++) {
-	// 	const element = document.createElement("div");
-	// 	element.classList.add("block-right");
-	// 	element.textContent = "TEST";
-	// 	sketchContainer3.appendChild(element);
-	// }
-	// for (let i = 0; i < 4; i++) {
-	// 	const element = document.createElement("div");
-	// 	element.classList.add("block-far-right");
-	// 	element.textContent = "TEST";
-	// 	sketchContainer3.appendChild(element);
-	// }
+	adjustBlockSize(totalBlocks);
 }
+const blockCount = sketchContainer.children.length;
+sketchContainer.style.setProperty("--block-count", blockCount);
 /**/
 
 createSketchPage();
