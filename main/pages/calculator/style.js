77e3929c3f6a2f999e6b2c15af0872ab88e4f1c2
Legendfox <rocketheadvids@gmail.com>
2024-11-20 13:04:12 -0600

    did things

--- main/pages/calculator/style.js
+++ main/pages/calculator/style.js
@@ -1,10 +1,33 @@
-for (let i = 0; i < 4; i++) {
-	let gridNum = i + 1;
-	let className = "grid-token-" + gridNum;
-	for (let i = 0; i < 16; i++) {
-		let container = document.querySelector("#grid");
+function createPage() {
+	for (let i = 0; i < 4; i++) {
+		let gridNum = i + 1;
+		let className = "grid-token-" + gridNum;
+
+		for (let i = 0; i < 4; i++) {
+			//let container = document.querySelector("#grid");
+			let elementBox = document.querySelector("#box-" + gridNum);
+			let element = document.createElement("div");
+			elementBox.appendChild(element);
+			element.classList.add("grid-token");
+			element.classList.add(className);
+		}
+	}
+
+	for (let i = 0; i < 1; i++) {
+		let elementBox = document.querySelector("#box-5");
 		let element = document.createElement("div");
-		container.appendChild(element);
-		element.classList.add("grid-token" + className);
+		elementBox.appendChild(element);
+		element.classList.add("grid-token");
+		element.classList.add("grid-token-manual");
+		element.classList.add("zero");
+	} //done in a for loop so that "element" remains independent
+
+	for (let i = 0; i < 2; i++) {
+		let elementBox = document.querySelector("#box-5");
+		let element = document.createElement("div");
+		elementBox.appendChild(element);
+		element.classList.add("grid-token");
+		element.classList.add("grid-token-manual");
 	}
 }
+createPage();
