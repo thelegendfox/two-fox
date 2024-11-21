778751d71bf5dfc50f531259d9af67c6b96d3560
Legendfox <rocketheadvids@gmail.com>
2024-11-21 12:13:59 -0600

    finished calc buttons

--- main/pages/calculator/style.js
+++ main/pages/calculator/style.js
@@ -3,7 +3,7 @@ function createPage() {
 		let gridNum = i + 1;
 		let className = "grid-token-" + gridNum;
 
-		for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
 			//let container = document.querySelector("#grid");
 			let elementBox = document.querySelector("#box-" + gridNum);
 			let element = document.createElement("div");
@@ -13,13 +13,18 @@ function createPage() {
 		}
 	}
 
-	for (let i = 0; i < 1; i++) {
+	for (let i = 0; i < 2; i++) {
 		let elementBox = document.querySelector("#box-5");
 		let element = document.createElement("div");
 		elementBox.appendChild(element);
-		element.classList.add("grid-token");
+		//element.classList.add("grid-token");
 		element.classList.add("grid-token-manual");
 		element.classList.add("zero");
+		if (i == 1) {
+			element.classList.add("zero-right");
+		} else {
+			element.classList.add("zero-left");
+		}
 	} //done in a for loop so that "element" remains independent
 
 	for (let i = 0; i < 2; i++) {
