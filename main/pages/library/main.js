f908d25f38a1bb34ad5c7368976dc6076e194c33
Legendfox <rocketheadvids@gmail.com>
2025-03-05 10:14:19 -0600

    added a bunch of stuff, near completion now :D

--- main/pages/library/main.js
+++ main/pages/library/main.js
@@ -14,11 +14,11 @@ function escape(input) {
 		.replace(/\//g, "&#x2F;");
 }
 
-let bookTitle = document.querySelector("#title");
-let bookAuthor = document.querySelector("#author");
-let bookReleaseDate = document.querySelector("#release");
-let bookPageNum = document.querySelector("#pagenum");
-let bookIsRead = document.querySelector("#read");
+let bookTitle = null;
+let bookAuthor = null;
+let bookReleaseDate = null;
+let bookPageNum = null;
+let bookIsRead = null;
 let bookSubmit = document.querySelector("#submit");
 let form = document.querySelector("#input-form");
 
@@ -30,6 +30,54 @@ function Book(title, author, release, pages, read) {
 	this.read = read;
 }
 
-form.addEventListener("submit", function (event) {
+bookSubmit.addEventListener("click", function (event) {
 	event.preventDefault();
+	bookTitle = document.querySelector("#title").value;
+	bookAuthor = document.querySelector("#author").value;
+	bookReleaseDate = document.querySelector("#release").value;
+	bookPageNum = document.querySelector("#pagenum").value;
+	bookIsRead = document.querySelector("#read").checked;
+
+	if (
+		bookTitle.length > 20 ||
+		bookAuthor.length > 20 ||
+		bookReleaseDate.length > 20 ||
+		bookPageNum.length > 20
+	) {
+		alert("Values cannot exceed 20 characters.");
+		console.log("Alert sent.");
+		return;
+	}
+
+	addBook(
+		new Book(bookTitle, bookAuthor, bookReleaseDate, bookPageNum, bookIsRead)
+	);
+	console.log(bookTitle);
 });
+
+function addBook(book) {
+	const bookContainer = document.querySelector("#books-list");
+	const element = document.createElement("div");
+
+	bookContainer.appendChild(element);
+	element.classList.add("book");
+
+	let title = book.title + "\xa0".repeat(20 - book.title.length);
+	let author = book.author + "\xa0".repeat(20 - book.author.length);
+	let release = book.release + "\xa0".repeat(20 - book.release.length);
+	let pages = book.pages + "\xa0".repeat(20 - book.pages.length);
+	// let read = book.read + "\xa0".repeat(19 - book.read.length);
+	// adds spaces to the title so it's always 20~ chars
+
+	element.textContent =
+		"Title: " +
+		title +
+		" | Author: " +
+		author +
+		" | Release Date: " +
+		release +
+		" | Pages: " +
+		pages +
+		" | Is Read: " +
+		book.read;
+}
